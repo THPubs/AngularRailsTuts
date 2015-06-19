78e5271487bfcbb328aaa9be0167db8eebcd50aa
Pubudu Kodikara <pubudu.th@gmail.com>
2015-06-19 09:11:26 +0530

    Form complete

--- app/assets/javascripts/directory.js
+++ app/assets/javascripts/directory.js
@@ -16,4 +16,13 @@ angular.module('directoryApp', [])
             name: 'Lam',
             age: 26
         }];
+
+        $scope.addPerson = function() {
+            $scope.list.push({
+                name: $scope.name,
+                age: $scope.age
+            });
+            $scope.name = '';
+            $scope.age = 0;
+        };
     });
